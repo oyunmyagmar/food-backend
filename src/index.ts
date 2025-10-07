dbff843d633599a756d03dbf7d5e5a26028bf41f
oyunmyagmar <25LP4018@nest.edu.mn>
2025-10-07 20:53:54 +0800

    added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import express, { Application, Request, Response } from "express";
-
 import cors from "cors";
 
 const app: Application = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!dfdggfdgfdgdfgdf 1dfdgdsgdf23");
+  res.send("Hello Word 123");
+});
+
+app.post("/create-food", (req: Request, res: Response) => {
+  console.log(req.body);
+  if (req.body == undefined) {
+    res.status(400).send("Error");
+  }
+  res.status(200).send("Success");
+});
+
+app.post("/create-category", (req: Request, res: Response) => {
+  console.log(req.body);
+  if (req.body == undefined) {
+    res.status(400).send("Error");
+  }
+  res.status(200).send("Success");
 });
 
 app.listen(port, () => {
